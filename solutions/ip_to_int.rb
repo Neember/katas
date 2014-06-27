c056e6a1f7bb2dff14789bdb6c629b23ca34ca56
Steven Yap <stevenyap@futureworkz.com>
2014-06-27 13:51:00 +0800

    Update ip_to_int solution to use refinement

--- solutions/ip_to_int.rb
+++ solutions/ip_to_int.rb
@@ -1,17 +1,21 @@
+module StringBinary
+  refine String do
+    def to_8_bits_string
+      self.to_i.to_s(2).rjust(8, '0')
+    end
+
+    def octets
+      self.split('.')
+    end
+  end
+end
+
+using StringBinary
+
 def ip_to_int32(ip)
   concat_binaries(ip.octets).to_i(2)
 end
 
 def concat_binaries(ip_octets)
   ip_octets.inject('') { |binary_str, ip_octet| binary_str += ip_octet.to_8_bits_string  }
-end
-
-class String
-  def to_8_bits_string
-    self.to_i.to_s(2).rjust(8, '0')
-  end
-
-  def octets
-    self.split('.')
-  end
 end
